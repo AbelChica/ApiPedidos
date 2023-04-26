dc49def637777b0714ae0428964f1d26c3d82717
AbelChica <chicaabel@hotmail.com>
2023-04-26 12:54:55 -0300

    feat: Desenvolver a pagina de documentação de do swager

---
+++ ApiPedidos.Services/Configurations/AddSwaggerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace ApiPedidos.Services.Configurations
+{
+    public class SwaggerConfiguration
+    {
+        public static void AddSwagger(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "API para controle de pedidos ",
+                        Version = "v1",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "COTI Informática",
+                            Email = "contato@cotiinformatica.com.br",
+                            Url = new Uri("http://www.cotiinformatica.com.br")
+                        }
+                    });
+            });
+        }
+    }
+}
+
+
+
--- ApiPedidos.Services/Program.cs
+++ ApiPedidos.Services/Program.cs
@@ -9,10 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+SwaggerConfiguration.AddSwagger(builder);
 DependencyInjectionConfiguration.AddDependencyInjection(builder);
 CorsConfiguration.AddCors(builder);
 
@@ -33,6 +31,3 @@ app.MapControllers();
 app.Run();
 
 public partial class Program { }
-
-
-
